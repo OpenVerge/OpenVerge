8971957616a0ce68caad96facad2abbceaccf7bf
Visalan H <visalanprivate@gmail.com>
2025-11-04 09:56:20 +0530

    Initial Commit

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import app from './app'
+
+const PORT = process.env.PORT || 3000
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'API is working' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
